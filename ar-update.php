f0c0ca042a206b8cece3addda8ee11f92c2e43f2
Ian Morris Nieves <imnieves@gmail.com>
2018-07-06 15:16:50 -0700

    added support for date_received, payment_method, back_account_id into the CSV

--- ar-update.php
+++ ar-update.php
@@ -2,20 +2,25 @@
 
 require __DIR__ . '/bootstrap.php';
 
-use Intacct\Functions\AccountsReceivable\CustomerCreate;
-use Intacct\Functions\AccountsReceivable\CustomerDelete;
-use Intacct\Functions\AccountsReceivable\CustomerUpdate;
-use Intacct\Functions\Common\Read;
-use Intacct\Functions\Common\ReadByName;
+use Intacct\Functions\AccountsReceivable\ArPaymentCreate;
 
-// STATE
+// ROW NAMES
 $paymentAmountRowName = 'payment_amount';
 $customerAccountIdRowName = 'customer_account_id';
+$dateReceivedRowName = 'date_received';
+$paymentMethodRowName = 'payment_method';
+$bankAccountIdRowName = 'bank_account_id';
+// ROW IDS
+$paymentAmountRowIndex = NULL;
+$customerAccountIdRowIndex = NULL;
+$dateReceivedRowIndex = NULL;
+$paymentMethodRowIndex = NULL;
+$bankAccountIdRowIndex = NULL;
+// FILE STATE
 $csvFilePath = NULL;
 $csvFileHandle = NULL;
 $csvFileDataRow = NULL;
-$paymentAmountRowIndex = NULL;
-$customerAccountIdRowIndex = NULL;
+// DATA STATE
 $dataRows = NULL;
 
 // GET CSV FILE PATH
@@ -45,6 +50,15 @@ for($i=0; $i<count($csvFileDataRow); $i++){
     else if($csvFileDataCell === $customerAccountIdRowName){
         $customerAccountIdRowIndex = $i;
     }
+    else if($csvFileDataCell === $dateReceivedRowName){
+        $dateReceivedRowIndex = $i;
+    }
+    else if($csvFileDataCell === $paymentMethodRowName){
+        $paymentMethodRowIndex = $i;
+    }
+    else if($csvFileDataCell === $bankAccountIdRowName){
+        $bankAccountIdRowIndex = $i;
+    }
 }
 if($paymentAmountRowIndex === NULL){
     exit('Error: unable to find column named: ' . $paymentAmountRowName . "\n");
@@ -52,18 +66,29 @@ if($paymentAmountRowIndex === NULL){
 else if($customerAccountIdRowIndex === NULL){
     exit('Error: unable to find column named: ' . $customerAccountIdRowName . "\n");
 }
+else if($dateReceivedRowIndex === NULL){
+    exit('Error: unable to find column named: ' . $dateReceivedRowName . "\n");
+}
+else if($paymentMethodRowIndex === NULL){
+    exit('Error: unable to find column named: ' . $paymentMethodRowName . "\n");
+}
+else if($bankAccountIdRowIndex === NULL){
+    exit('Error: unable to find column named: ' . $bankAccountIdRowName . "\n");
+}
 
 // READ AND VALIDATE ROWS
 $rowIndex = 2;
 $rowReadCount = 0;
-$rowSkipCount = 0;
 $dataRows = array();
 while(($csvFileDataRow = fgetcsv($csvFileHandle)) !== FALSE){
-    $paymentAmount = trim($csvFileDataRow[$paymentAmountRowIndex]);
     $customerAccountId = trim($csvFileDataRow[$customerAccountIdRowIndex]);
-    // VERIFY BOTH FIELDS ARE NOT EMPTY
+    $paymentAmount = trim($csvFileDataRow[$paymentAmountRowIndex]);
+    $dateReceived = trim($csvFileDataRow[$dateReceivedRowIndex]);
+    $paymentMethod = trim($csvFileDataRow[$paymentMethodRowIndex]);
+    $bankAccountId = trim($csvFileDataRow[$bankAccountIdRowIndex]);
+    // VERIFY ALL FIELDS ARE NOT EMPTY
     if(strlen($paymentAmount) > 0
-        || strlen($customerAccountId) > 0){
+        && strlen($customerAccountId) > 0){
         // VERIFY PAYMENT AMOUNT IS NUMERIC
         if(!is_numeric($paymentAmount)){
             exit('Error: payment amount is not a number, at row: ' . $rowIndex . "\n");
@@ -72,13 +97,16 @@ while(($csvFileDataRow = fgetcsv($csvFileHandle)) !== FALSE){
             $rowReadCount++;
             // ADD ROW TO ROWS
             $dataRow = array();
-            $dataRow[$paymentAmountRowName] = $paymentAmount;
             $dataRow[$customerAccountIdRowName] = $customerAccountId;
+            $dataRow[$paymentAmountRowName] = $paymentAmount;
+            $dataRow[$dateReceivedRowName] = $dateReceived;
+            $dataRow[$paymentMethodRowName] = $paymentMethod;
+            $dataRow[$bankAccountIdRowName] = $bankAccountId;
             $dataRows[] = $dataRow;
         }
     }
     else{
-        $rowSkipCount++;
+        exit('Error: a field is empty, at row: ' + $rowIndex . "\n");
     }
     // INCREMENT ROW COUNTER
     $rowIndex++;
@@ -86,12 +114,51 @@ while(($csvFileDataRow = fgetcsv($csvFileHandle)) !== FALSE){
 fclose($csvFileHandle);
 echo "data row stats\n";
 echo "\tread: " . $rowReadCount . "\n";
-echo "\tempty: " . $rowSkipCount . "\n";
 
 // EXIT IF NOTHING READ
 if(count($dataRows) === 0){
     exit("Error: no rows read\n");
 }
 
-// PROCESS ROWS
-// ...
+
+try {
+    // PROCESS EACH ROW
+    foreach($dataRows as $dataRow) {
+        // CREATE NEW AR PAYMENT
+        $customerAccountId = $dataRow[$customerAccountIdRowName];
+        $paymentAmount = $dataRow[$paymentAmountRowName];
+        $dateReceived = $dataRow[$dateReceivedRowName];
+        $paymentMethod = $dataRow[$paymentMethodRowName];
+        $bankAccountId = $dataRow[$bankAccountIdRowName];
+        $arPaymentCreate = new ArPaymentCreate();
+        $arPaymentCreate->setCustomerId($customerAccountId);
+        $arPaymentCreate->setTransactionPaymentAmount($paymentAmount);
+        $arPaymentCreate->setReceivedDate(new DateTime($dateReceived));
+        $arPaymentCreate->setPaymentMethod(constant("Intacct\Functions\AccountsReceivable\ArPaymentCreate::$paymentMethod"));
+        $arPaymentCreate->setBankAccountId($bankAccountId);
+        // EXECUTE
+        $logger->info('Executing query to Intacct API');
+        $response = $client->execute($arPaymentCreate);
+        $result = $response->getResult();
+        // LOG RESULT
+        $logger->debug('Query successful', [
+            'Company ID' => $response->getAuthentication()->getCompanyId(),
+            'User ID' => $response->getAuthentication()->getUserId(),
+            'Request control ID' => $response->getControl()->getControlId(),
+            'Function control ID' => $result->getControlId(),
+            'Total count' => $result->getTotalCount(),
+            'Data' => json_decode(json_encode($result->getData()), 1),
+        ]);
+    }
+} catch (\Intacct\Exception\ResponseException $ex) {
+    $logger->error('An Intacct response exception was thrown', [
+        get_class($ex) => $ex->getMessage(),
+        'Errors' => $ex->getErrors(),
+    ]);
+    echo 'Failed! ' . $ex->getMessage();
+} catch (\Exception $ex) {
+    $logger->error('An exception was thrown', [
+        get_class($ex) => $ex->getMessage(),
+    ]);
+    echo get_class($ex) . ': ' . $ex->getMessage();
+}
